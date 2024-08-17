3159cf2846ec82794c50bbc3458f7bf4eb993238
Nathan Hotchkin <15834648+nphotchkin@users.noreply.github.com>
2024-08-17 15:30:08 +0200

    customize the script

--- ship.sh
+++ ship.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# <-- PROMPT FOR INPUT -->
+
+# Define color codes
+COLOR_RESET="\033[0m"
+COLOR_RED="\033[0;31m"
+COLOR_GREEN="\033[0;32m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_BLUE="\033[0;34m"
+COLOR_MAGENTA="\033[0;35m"
+COLOR_CYAN="\033[0;36m"
+
+# Print the ASCII boat in rainbow colors
+echo -e "${COLOR_RED}  _________.__    .__        ${COLOR_RESET}"
+echo -e "${COLOR_ORANGE}/   _____/|  |__ |__|_____  ${COLOR_RESET}"
+echo -e "${COLOR_YELLOW}\_____  \ |  |  \|  \____ \ ${COLOR_RESET}"
+echo -e "${COLOR_GREEN}/        \|   Y  \  |  |_> > ${COLOR_RESET}"
+echo -e "${COLOR_CYAN}/_______  /|___|  /__|   __/ ${COLOR_RESET}"
+echo -e "${COLOR_BLUE}        \/      \/   |__|    ${COLOR_RESET}"
+echo -e "${COLOR_PURPLE}                             ${COLOR_RESET}"
+
+# Print a colored prompt and read user input
+read -p "$(echo -e "${COLOR_CYAN}Enter a summary of the changes make? ${COLOR_RESET}")" SUMMARY
+
+# Print a personalized message
+echo -e "${COLOR_GREEN}The following changes have been made: \n\n ${SUMMARY}!${COLOR_RESET}"
+
+# <-- PROMPT FOR INPUT -->
+
 # Check if version number is passed as an argument
 if [ -z "$1" ]; then
   echo "Error: No version number supplied."
@@ -11,7 +39,7 @@ fi
 VERSION=$1
 TARGET_REPO="git@github.com:nphotchkin/target-project.git"  # Replace with your target repository
 TARGET_BRANCH="main"  # Replace with the default branch name of your target repository
-
+GITHUB_USERNAME=$(gh api user | jq -r '.login')
 
 # Get the current branch name
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -22,9 +50,6 @@ git fetch origin
 # Get the commit summary of commits on the current branch that are not in other branches
 # This includes commits that are unique to your current branch
 COMMIT_SUMMARY=$(git log --oneline --no-merges --cherry-pick origin/main..$CURRENT_BRANCH)
-
-
-
 
 
 #### PUSH TO TARGET
@@ -52,7 +77,10 @@ git push origin "$VERSION"
 PR_BODY=$(cat <<EOF
 ## Release $VERSION
 
-This pull request includes the files for version $VERSION.
+$GITHUB_USERNAME has created a new release version $VERSION.  
+
+### Developer Description Of Changes:
+$SUMMARY
 
 ### Summary of commits from source repository:
 $COMMIT_SUMMARY
